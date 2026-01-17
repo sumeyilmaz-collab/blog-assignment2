e33bdf2d73f55c651e6bb6491ba686c006f3fe42
Sumeyye <sumeyilmaz@gmail.com>
2026-01-17 03:21:02 +0300

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  output: "server",          // Supabase SSR sayfalar için
+  adapter: netlify(),        // Netlify adapter
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
