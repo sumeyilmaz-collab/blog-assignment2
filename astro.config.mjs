72766c5e31d4dea6cb072c26180f8f1757d60743
Sumeyye <sumeyilmaz@gmail.com>
2026-01-17 03:42:25 +0300

    modified:   astro.config.mjs 	modified:   src/layouts/MainLayout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  output: "server",          // Supabase SSR sayfalar için
-  adapter: netlify(),        // Netlify adapter
+  output: "server",
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
